c6b97d5d826538001ee7064bf282bdbbf6d6f6c5
Glitch (hello-express) <none>
2020-11-27 04:37:31 +0000

    🏮🍠 Checkpoint ./server.js:25557114/4902 ./config.json:25557114/59 ./commands/corona.js:25557114/1836 ./package.json:25557114/1562

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-//UPTIME ROBOT (WEB)
-const { get } = require("snekfetch");
-const http = require("http");
+const discord = require("discord.js");
+const client = new discord.Client({disableMentions:"everyone"})
+const db = new Map()
+const fs = require("fs")
+const snek = require("node-superfetch")
+
 const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
@@ -10,48 +11,84 @@ app.get("/", (request, response) => {
   response.sendStatus(200);
 })
 app.listen(process.env.PORT);
-setInterval(() => {
-http.get('http://discord-tutori4l.glitch.me/');
-}, 280000);
-client.on("ready", async () => {
-  console.log(`${client.user.tag} sudah online!`);
-  client.user.setActivity("With Everyone");
-});
-client.on("message", async message => {
-  if (message.content === "indonesia") message.reply(":flag_id:");
-//COMMAND BOT DI SERVER.JS
-const prefix = "!"
-if(!message.content.startsWith(prefix)) return null;
-let msg = message.content.toLowerCase();
-let args = message.content.slice(prefix.length).trim().split(" ");
-let cmd = args.shift().toLowerCase();
-let command = cmd;
 
-let commandFiles;
-try{
-  commandFiles = require(`./commands/${cmd}.js`)
-} catch (err) {
-  return message.reply("Command Not Found")
+const { prefix } = require("./config.json")
+client.aliases = new discord.Collection();
+client.commands = new discord.Collection();
+client.prefix = prefix
+client.queue = new Map()
+client.hastebin = async(text) => {
+  const { body } = await snek.post("https://bin-clientdev.glitch.me/documents")
+  .send(text);
+  return `https://bin-clientdev.glitch.me/${body.key}`
 }
-const db = require("quick.db")
-const now = Date.now()
-if(db.has(`cd_${message.author.id}`)) {
-  const expirationTime = db.get(`cd_${message.author.id}`) + 3000
-  if(now < expirationTime) {
-  const timeLeft = (expirationTime - now) / 1000;
-		return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${cmd}\` command.`);
+
+//event
+client.on('ready', () => {
+  console.log(`${client.user.tag} ready to serving ${client.guilds.cache.size} guild(s) and ${client.users.cache.size} user(s), with ${client.commands.size} command(s) total!`)
+  client.user.setActivity("All of you!", {type:"WATCHING"})
+});
+
+const commandFile = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+commandFile.forEach(file => {
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.name, command);
+  if(command.alias) {
+  command.alias.forEach(alias => {
+  client.aliases.set(alias, command);
+  })
   }
+  console.log(`Loaded command ${command.name} with alias(es) => ${command.alias}`)
+  })
+
+client.on('message', msg => {
+  if(msg.author.bot) return;
+  if(!msg.guild) return;
+  
+  if(msg.content == `<@${client.user.id}>`){
+    const embed = new discord.MessageEmbed()
+    .setDescription(`:wave: | My prefix is ${prefix}`)
+    .setColor("YELLOW")
+    .setFooter("Nordd Universe 2020")
+    msg.channel.send(embed)
+  }
+  if(msg.content == prefix) {
+    const embed = new discord.MessageEmbed()
+    .setDescription(`Hey, It's me!
+You can type ${prefix}help to get bot commands list`)
+    .setColor("YELLOW")
+    .setFooter("Nordd Universe 2020")
+    return msg.channel.send(embed)
+  }
+  let args = msg.content.slice(prefix.length).trim().split(" ");
+  let cmd = args.shift().toLowerCase();
+  if(!msg.content.startsWith(prefix)) return;
+  
+  try {
+    const file = client.commands.get(cmd) || client.aliases.get(cmd)
+    if(!file) return msg.reply("Perintah yang Anda inginkan tidak ada.")
+    
+    const now = Date.now()
+   if (db.has(`cooldown_${msg.author.id}`)) {
+	const expirationTime = db.get(`cooldown_${msg.author.id}`) + 3000;
+	if (now < expirationTime) {
+		const timeLeft = (expirationTime - now) / 1000;
+		return msg.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${file.name}\` command.`);
+	}
 }
-  db.set(`cd_${message.author.id}`, now);
+  db.set(`cooldown_${msg.author.id}`, now);
   setTimeout(() => {
-    db.delete(`cd_${message.author.id}`)
+    db.delete(`cooldown_${msg.author.id}`)
   },3000)
-try {
-  commandFiles.run(client, message, args)
-} catch (err) {
+    
+    file.run(client, msg, args)
+  } catch (err) {
+    console.error(err)
   } finally {
-    console.log(`${message.author.tag} menggunakan command ${prefix}${cmd}`)
+    console.log(`${msg.author.tag} using ${cmd} in ${msg.channel.name} | ${msg.guild.name}`)
   }
+}) 
 
-});
-client.login(process.env.TOKEN);
+
+//insert token at .env first
+client.login(process.env.TOKEN)
