dafea8d7a1522a447c816d67b8e1fa65008ae4c9
Glitch (hello-express) <none>
2020-12-01 13:31:21 +0000

    💽🌎 Checkpoint ./commands/embedsay.js:25557114/657 ./watch.json:25557114/8 ./commands/say.js:25557114/324

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,9 @@
-exports.run = async(client, message, args) => {
+module.exports = {
+  name: "say",
+  alias: ["say", "sy"],
+  description: "to say message",
+run: async(client, message, args) => {
   let say = args.join(' ')
   message.channel.send(say)
+}
 }
